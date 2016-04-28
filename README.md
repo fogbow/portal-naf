382a5e37d69f96f840c29895a6d0aabd2397ab44
Jefferson Neves <jefferson.rpn@gmail.com>
2016-04-28 18:46:04 -0300

    Setup deployment

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# portal-naf
+# Portal da Nuvem Acadêmica Federada
+
+## How to contribute
+
+To setup dev environment, you need **node**, **npm**, **bower** and **gulp** pre-installed.
+
+Install dependencies:
+
+```bash
+npm install
+gulp install
+```
+
+Run:
+
+```bash
+gulp serve
+```
--- gulpfile.js
+++ gulpfile.js
@@ -8,13 +8,15 @@ var
   concat = require('gulp-concat'),
   uglify = require('gulp-uglify'),
   sourcemaps = require('gulp-sourcemaps'),
-  del = require('del');
+  del = require('del'),
+  inject = require('gulp-inject'),
+  browserSync = require('browser-sync').create();
 
 var paths = {
+  src: './src',
   dist: './dist',
   sass: './src/scss',
-  bower: './bower_components',
-  scripts: './src/js'
+  bower: './bower_components'
 }
 
 var showErrorMsg = function(error) {
@@ -30,15 +32,36 @@ gulp.task('clean', function() {
   return del([paths.dist]);
 });
 
-gulp.task('sass', function () {
+gulp.task('sass', function() {
   return gulp.src(paths.sass+'/*.scss')
     .pipe(sass({outputStyle: 'compressed'})
       .on('error', sass.logError))
-    .pipe(gulp.dest(paths.dist+'/assets/css'));
+    .pipe(gulp.dest(paths.src+'/css'));
 });
 
-gulp.task('scripts', ['clean'], function () {
-  return gulp.src(paths.scripts+'/*.js')
+gulp.task('serve', ['sass'], function() {
+    browserSync.init({
+        server: {
+            baseDir: paths.src
+        }
+    });
+    gulp.watch(paths.sass+'/*.scss', ['sass']);
+    gulp.watch(paths.src+"/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('build', function() {
+  var target = gulp.src(paths.src+'/index.html');
+  var sources = gulp.src([
+    paths.bower+'/jquery/dist/jquery.min.js',
+    paths.bower+'/bootstrap-sass/assets/javascripts/bootstrap.min.js',
+    paths.src+'/js/*.js',
+    paths.src+'/css/*.css'], {read: false, relative: false});
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest(paths.src));
+});
+
+gulp.task('dist-scripts', function() {
+  return gulp.src(paths.src+'/js/*.js')
     .pipe(sourcemaps.init())
     .pipe(uglify())
     .pipe(concat('bundle.min.js'))
@@ -46,9 +69,34 @@ gulp.task('scripts', ['clean'], function () {
     .pipe(gulp.dest(paths.dist+'/assets/js'));
 });
 
+gulp.task('dist-scripts', function() {
+  return gulp.src(paths.src+'/js/*.js')
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(concat('bundle.min.js'))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(paths.dist+'/assets/js'));
+});
+
+gulp.task('dist-scripts-vendor', function() {
+  return gulp.src([
+        paths.bower+'/jquery/dist/jquery.min.js',
+        paths.bower+'/bootstrap-sass/assets/javascripts/bootstrap.min.js'])
+    .pipe(gulp.dest(paths.dist+'/assets/js'));
+});
+
+gulp.task('dist-css', function() {
+  return gulp.src(paths.src+'/css/*.css')
+    .pipe(gulp.dest(paths.dist+'/assets/css'));
+});
+
+gulp.task('dist', ['sass', 'dist-scripts', 'dist-scripts-vendor', 'dist-css'], function() {
+  return gulp.src(paths.src+'/index.html')
+      .pipe(gulp.dest(paths.dist));
+});
+
 gulp.task('watch', function() {
   gulp.watch(paths.sass+'/*.scss', ['sass']);
-  gulp.watch(paths.scripts+'/*.js', ['scripts']);
 });
 
 gulp.task('default', ['scripts', 'sass', 'watch']);
